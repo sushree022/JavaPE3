cfae83a5417958563b8bb7f49ba89a480f361c3f
sushree mishra <sushree022@gmail.com>
2019-07-19 14:10:56 +0530

    test case for date check added

--- src/main/java/com/stackroute/pe3/DateOfWeek.java
+++ src/main/java/com/stackroute/pe3/DateOfWeek.java
@@ -1,28 +1,6 @@
-//package com.stackroute.pe3;
-//
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//public class DateOfWeek {
-//    DateOfWeek dateOfWeek;
-//    /* This methods runs, before running any one of the test case .
-//          It is used to initialize the required variables
-//   */
-//    @Before
-//    public void setup()
-//    {
-////    arrange
-//        dateOfWeek= new DateOfWeek();
-//    }
-//    /* This method runs, after running all the test cases
-//           It is used to clear the initialized variables
-//    */
-//    @After
-//    public void teardown()
-//    {
-//        dateOfWeek=null;
-//    }
-//    @Test
-//    public void given
-//}
+package com.stackroute.pe3;
+
+
+public class DateOfWeek {
+
+}
--- src/test/java/com/stackroute/pe3/DateOfWeekTest.java
+++ src/test/java/com/stackroute/pe3/DateOfWeekTest.java
@@ -1,8 +1,38 @@
-//package com.stackroute.pe3;
-//
-//import static org.junit.Assert.*;
-//
-//public class DateOfWeekTest {
-//    DateOfWeek dateOfWeek;
-//}
-//
+package com.stackroute.pe3;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DateOfWeekTest {
+    DateOfWeek dateOfWeek;
+    /* This methods runs, before running any one of the test case .
+          It is used to initialize the required variables
+   */
+    @Before
+    public void setup()
+    {
+//    arrange
+        dateOfWeek= new DateOfWeek();
+    }
+    /* This method runs, after running all the test cases
+           It is used to clear the initialized variables
+    */
+    @After
+    public void teardown()
+    {
+        dateOfWeek=null;
+    }
+    @Test
+    public void givenStartAndEndDateShouldReturnAStringMessage()
+    {
+//        act
+        String actualresult= dateOfWeek.calculateDate();
+        String expectedresult= ("Mon 14/07/2019 Sun 21/07/2019");
+//        assert
+        assertEquals(expectedresult,actualresult);
+    }
+}
+
